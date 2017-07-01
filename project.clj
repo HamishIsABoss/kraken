baf0204b759d82dab0fed7eacbab0ee66f35ea58
HamishIsABoss <hmccreanor1@tribeig.com>
2017-07-01 10:59:36 +0400

    Added basic API functions
    Added functions to get bulk match data, single match data, bulk player data and single player data.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject kraken "1.0.0"
+  :description "A wrapper for the Vainglory API"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [cheshire "5.7.1"]
+                 [clj-http-lite "0.3.0"]])
+
---
+++ src/kraken/core.clj
@@ -0,0 +1,65 @@
+(ns kraken.core
+  (:require [clj-http.lite.client :as client]
+            [cheshire.core :refer :all]))
+
+;; Utilities
+
+(defn- url
+  [region endpoint]
+  (str "https://api.dc01.gamelockerapp.com/shards/" region "/" endpoint))
+
+
+(defn- headers
+  [api-key]
+  {"Authorization" api-key
+   "Accept" "application/vnd.api+json"})
+
+
+;; API
+
+(defn api-response
+  [url headers & params]
+  (client/get url {:headers headers
+                   :params params}))
+
+
+(defn- plural-endpoint
+  [endpoint]
+  (fn [api-key region & params]
+    (->
+      (url region endpoint)
+      (api-response (headers api-key) params)
+      :body
+      (parse-string true))))
+
+
+(defn- singular-endpoint
+  [endpoint]
+  (fn [api-key region id]
+    (->
+      (url region (str endpoint "/" id))
+      (api-response (headers api-key))
+      :body
+      (parse-string true))))
+
+;; Endpoints
+
+(def matches
+  (plural-endpoint "matches"))
+
+
+(def match
+  (singular-endpoint "matches"))
+
+
+(def players
+  (plural-endpoint "players"))
+
+
+(def player
+  (singular-endpoint "players"))
+
+
+(def samples
+  (plural-endpoint "samples"))
+
